e908c717596d1e38cf6d1453bb7a7b5388e5e76a
Matthias Pitscher <schaefer9111@gmail.com>
2016-12-20 01:44:42 +0100

    fix bug in Time / add points

--- js/timeforthesis.js
+++ js/timeforthesis.js
@@ -1,27 +1,15 @@
-//Find out the time remaining without p5.js out of simplicity
 var end = new Date(2017, 3, 24); //(YEAR,MONTH,DATE,HOUR,MINUTE,SECOND,MILLIS)
 var days, minutes, seconds, hours, distance;
 var _second = 1000;
 var _minute = _second * 60;
 var _hour = _minute * 60;
 var _day = _hour * 24;
-
-function showRemaining() {
-	var now = new Date();
-	distance = end - now;
-	days = Math.floor(distance / _day - 30); //somehow the calculation is off by 30 days
-  hours = Math.floor((distance % _day) / _hour);
-	minutes = Math.floor((distance % _hour) / _minute);
-	seconds = Math.floor((distance % _minute) / _second);
-}
-
-
 var p5timer;
+
 function setup() {
 	p5timer = select('#time');
 	frameRate(1);
 	noCanvas();
-  showRemaining(); //not sure if necessary
 }
 
 function draw() {
@@ -29,55 +17,64 @@ function draw() {
 	if (distance <= 0) {
 		p5timer.html("I had to give out my Thesis already...");
 	} else {
-
+		//draw points
 		var countdays = "";
-		for (var i = 1; i < days + 1; i++) {
-			countdays += "#" + i + "";
+		for (var i = 1; i < days; i++) {
+			countdays += ".";
 		}
 		var counthours = "";
-		for (var i = 1; i < hours + 1; i++) {
-			counthours += "#" + i + "";
+		for (var i = 1; i < hours; i++) {
+			counthours += ".";
 		}
 		var countminutes = "";
-		for (var i = 1; i < minutes + 1; i++) {
-			countminutes += "#" + i + "";
+		for (var i = 1; i < minutes; i++) {
+			countminutes += ".";
 		}
 		var countseconds = "";
 		for (var i = 1; i < seconds + 1; i++) {
-			countseconds += "#" + i + "";
+			countseconds += ".";
 		}
 
     if (days > 0){
-      var daystring = "and " + days + " days remaining until I have to hand in my bachelor thesis... <br>"
+      var daystring = "and " + days + " days remaining until I have to hand in&nbsp;my&nbsp;bachelor&nbsp;thesis"
     }else{
-      var daystring = "remaining until I have to hand in my bachelor thesis..."
+      var daystring = "remaining until I have to hand in my bachelor thesis"
     }
 
     if (hours === 1){
-      var hourstring = hours + " hour</br>";
+      var hourstring = hours + "&nbsp;hour";
     } else{
-      var hourstring = hours + " hours</br>";
+      var hourstring = hours + "&nbsp;hours";
     }
 
     if (minutes === 1){
-      var minutestring = minutes + " minute</br>";
+      var minutestring = minutes + "&nbsp;minute";
     } else{
-      var minutestring = minutes + " minutes</br>";
+      var minutestring = minutes + "&nbsp;minutes";
     }
 
     if (seconds === 1){
-      var secondstring = seconds + " second </br>"
+      var secondstring = seconds + "&nbsp;second "
     } else{
-      var secondstring = seconds + " seconds </br>";
+      var secondstring = seconds + "&nbsp;seconds";
     }
 		p5timer.html(
-      distance / 1000 + "<br>"+
-			hourstring +
-			minutestring +
-			secondstring +
-			daystring
+			hourstring + counthours + "<br>" +
+			minutestring + countminutes + "<br>" +
+			secondstring + countseconds + "<br>" +
+			daystring + countdays
 		);
 
 	}
 
+}
+
+//Find out the time remaining without p5.js out of simplicity
+function showRemaining() {
+	var now = new Date(year(),month(),day(),hour(),minute(),second()); //I put the p5 functions inside because otherwise it would give me back a wrong string format
+	distance = end - now;
+	days = Math.floor(distance / _day);
+  hours = Math.floor((distance % _day) / _hour);
+	minutes = Math.floor((distance % _hour) / _minute);
+	seconds = Math.floor((distance % _minute) / _second);
 }
