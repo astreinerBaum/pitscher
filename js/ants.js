ce9f2bf4695a6fbbc114b0da68e603f53b8752dd
Matthias Pitscher <schaefer9111@gmail.com>
2017-11-01 22:58:18 +0100

    add new portfolio design

--- js/ants.js
+++ js/ants.js
@@ -1,7 +1,7 @@
 var ants = [];
 
 function setup() {
-	createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth, windowHeight);
 	for (var i = 0; i < 200; i++) {
 		ants.push(new Ant(createVector(random(width), random(height)), 3));
 	}
@@ -25,44 +25,44 @@ function Ant(location, radius) {
 	this.acceleration = createVector(0, 0.05);
 	this.maxspeed = 1;
 	this.history = [];
-	
-	this.run = function () {
+
+	this.run = function() {
 		this.update();
 		this.checkEdges();
 		this.randomPush(99);
 		this.display();
 	};
-	this.update = function () {
+	this.update = function() {
 		this.history.push(this.location);
 		if (this.history.length > 100) {
 			this.history.splice(0, 1);
 		}
-		
+
 		this.velocity.add(this.acceleration);
 		this.location.add(this.velocity);
 		this.velocity.limit(this.maxspeed);
 		this.acceleration.mult(0);
-		
-	};
-	this.display = function () {
-		fill(0);
-		ellipse(this.location.x, this.location.y, this.radius/1.2, this.radius/1.2);
-		ellipse(this.location.x - this.velocity.x * this.radius*2,
-				this.location.y - this.velocity.y * this.radius*2,
-				this.radius,
-				this.radius );
-		ellipse(this.location.x - this.velocity.x * (this.radius),
-				this.location.y - this.velocity.y * (this.radius),
-				this.radius/1.5,
-				this.radius/1.5 );
 
 	};
-	this.randomPush = function (percent) {
+	this.display = function() {
+		fill(0);
+		ellipse(this.location.x, this.location.y, this.radius / 1.2, this.radius / 1.2);
+		ellipse(this.location.x - this.velocity.x * this.radius * 2,
+			this.location.y - this.velocity.y * this.radius * 2,
+			this.radius,
+			this.radius);
+		ellipse(this.location.x - this.velocity.x * (this.radius),
+			this.location.y - this.velocity.y * (this.radius),
+			this.radius / 1.5,
+			this.radius / 1.5);
+
+	};
+	this.randomPush = function(percent) {
 		if (random(0, 1) > percent / 100) {
 			this.acceleration.add(random(-0.6, 0.6), random(-0.6, 0.6));
 		}
 	};
-	this.checkEdges = function () {
+	this.checkEdges = function() {
 		if (this.location.x + this.radius / 2 > width) this.velocity.x *= -1;
 		if (this.location.x - this.radius / 2 < 0) this.velocity.x *= -1;
 		if (this.location.y + this.radius / 2 > height) this.velocity.y *= -1;
