3ee8eb004eae810e0d46894fb0b39b31914d0327
Matthias S <schaefer9111@gmail.com>
2019-01-27 21:45:13 +0100

    

--- clicker/sketch.js
+++ clicker/sketch.js
@@ -1,30 +1,39 @@
+/*
+pitscher.net
+Clicker Game for
+Sankt Interface
+*/
+
+
 let num = 0;
 let countdown;
-let firstClick = false;
-let veryFirstClick = false;
-let yourScoreTimeOut = false;
 let stopCounting = false;
-let restartClick = false;
 let countdownSet = false;
+let firstClick   = false;
 
 let countdownDOM, numberDOM, additionalTextDOM, hatemel;
 let gameState, avgSpeed;
+let waitForRestart;
 
-let cnv;
+const COUNTDOWNSECONDS = 15
 
 function setup() {
-  //  cnv = createCanvas(windowWidth, windowHeight);
-  //  cnv.id('canvas');
-  noCanvas();
+  noCanvas(); // I interact directly with the DOM Elements
+
+  //Selecting all DOM Elements I want to change
   countdownDOM = select('#countdown');
   numberDOM = select('#clickerNumber');
   additionalTextDOM = select('#additionalText');
   highscoreDOM = select('#highscore');
   tryAgainBtn = select('#tryAgain');
+  tryAgainBtn.mouseClicked(startGame);
+
+  //Changing the cursor style
   hatemel = select('html');
   hatemel.addClass('cursor');
 
-  start();
+  startGame();
+  firstClick = true;
 }
 
 function draw() {
@@ -32,29 +41,14 @@ function draw() {
   if (gameState == 0) {
     num = 0;
     additionalTextDOM.html('CLICK TO START');
-    hatemel.mouseClicked(function () {
-      gameState++;
-      num++;
-    });
+    console.log("gamestate 000");
   }
 
   if (gameState == 1) {
-    if (!countdownSet) {
-      countdown = millis() + 15000;
-      countdownSet = true;
-    }
-
-    if (!stopCounting) {
-      hatemel.mouseClicked(function () {
-        num++;
-      });
-    }
-
     additionalTextDOM.html('&nbsp;');
     numberDOM.html(num.toString());
 
     let colorCountdown = map(countdownTimer(), 15, 0, 140, 0);
-    let cTime = ceil(countdownTimer());
     let decimalTime = countdownTimer().toString().split(".");
     if (decimalTime[1] == null) decimalTime[1] = "00";
     if (decimalTime[1] < 10) decimalTime[1] = "0" + decimalTime[1];
@@ -65,11 +59,12 @@ function draw() {
     countdownDOM.style('color', 'hsl(' + colorCountdown + ', 80%, 40%)')
     countdownDOM.html(decimalTime[0] + ":" + decimalTime[1].substr(0, 2));
 
-    if (countdownTimer() <= 0.001) {
+    if (countdownTimer() == 0) {
       gameState++;
+      waitForRestart = millis() + 2000;
       stopCounting = true;
       countdown = false;
-      avgSpeed = num / 15;
+      avgSpeed = num / COUNTDOWNSECONDS;
       highscore(num);
     }
   }
@@ -77,14 +72,16 @@ function draw() {
   if (gameState == 2) {
     countdownDOM.html('');
     additionalTextDOM.html('YOUR SCORE!</br><span class=\'small\'> ' + avgSpeed.toString().substr(0, 3) + ' clicks / second </span>');
-    tryAgainBtn.style('visibility', 'visible');
-    tryAgainBtn.mouseClicked(start);
+    
+    //wait for 2 seconds and make restart button visible
+    if(millis() >= waitForRestart){
+      tryAgainBtn.style('visibility', 'visible');
+    }
   }
 
 }
 
-
-function start() {
+function startGame() {
   num = 0;
   numberDOM.html(num.toString());
   additionalTextDOM.html('&nbsp;');
@@ -100,30 +97,66 @@ function start() {
 function countdownTimer() {
   let cd = countdown - millis();
   if (cd > 0) {
-    return cd / 1000;
+    return cd / 1000; //returns seconds instead of milliseconds
   } else {
     return 0
   }
 }
 
-// Make cursor to gif
+function setCountdown(s){
+  countdown = millis() + s*1000;
+}
+
+
 function mousePressed() {
+  //Change mouse style
   hatemel.addClass('cursor-down');
   hatemel.removeClass('cursor')
 }
 
 function mouseReleased() {
+  // change mouse style
   hatemel.addClass('cursor');
   hatemel.removeClass('cursor-down');
 }
 
-let highscoreList = [];
+function mouseClicked(){
+    if (gameState == 0 && firstClick == true){
+      gameState++;
+      //num++;
+    }
 
+    if (!firstClick){
+      firstClick = true;
+    }
+
+    if (gameState == 1) {
+      if (!countdownSet) {
+        setCountdown(COUNTDOWNSECONDS);
+        countdownSet = true;
+      }
+  
+      if (!stopCounting) {
+          num++;
+      }
+    }
+}
+
+
+let hsConstrain;
+let highscoreList = []; //This is how you define an empty array in JS
 function highscore(n) {
   highscoreList.push(n);
   highscoreList.sort(function(a, b){return b-a});
   let list = '';
-  for (let i = 0; i < highscoreList.length; i++) {
+
+  if(highscoreList.length >= 10){
+    hsConstrain = 10
+  } else {
+    hsConstrain = highscoreList.length;
+  }
+
+  for (let i = 0; i < hsConstrain; i++) {
     list += '<li>' + highscoreList[i] + '</li>';
   }
   highscoreDOM.html(list);
